28b8891549450d5366d4e40349954eeddefb37c0
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-10 17:37:12 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,8 +3,51 @@ import React from 'react';
 
 
 class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
 
-  
+      this.state = {
+        errors: [],
+        user: null,
+        settings: {
+          birate: 8,
+          video: {
+            resolution: '1080p' 
+           }
+        } 
+      };
+  }
+
+    handleBirate = () => {
+      this.setState({
+        settings: {
+          ...this.state.settings,
+          bitrate: 12
+        }
+      });
+    };
+
+    handleResolution = () => {
+      this.setState({
+        settings: {
+          ...this.state.settings,
+          video: {
+            ...this.state.settings.video,
+            resolution: '720p'
+          }
+        }
+      });
+    };
+
+    render() {
+      return (
+          <div>
+            <button className="birate" onClick={this.handleBirate}>
+            Change bitrate
+            </button>
+          </div>
+      );
+    }
 }
 
 export default YouTubeDebugger;
