2c148a2f992ad8a32de1e0d0969c1e6fab22eacb
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-10 16:38:38 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,9 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,10 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+
+class YouTubeDebugger extends React.Component {
+
+  
+}
+
+export default YouTubeDebugger;
