72c379beb24dda8d9e2c23d9e9cbe6b239148065
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-10 17:40:49 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,14 +2,13 @@
 import React from 'react';
 
 class DigitalClicker extends React.Component {
-    constructor() {
+  constructor() {
     super();
-
-    this.state() = {
-            timesClicked: 0
+    this.state = {
+      timesClicked: 0
     };
+  }
 
-}
 
   handleClick = () => {
     this.setState(prevState => ({
